51127226540529674340ceb1acf5b39ea2c452e7
ykarmr <yuki.arimura0508@gmail.com>
2024-07-31 18:20:45 +0900

    レイアウトの修正

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,14 +8,14 @@ interface LayoutProps {
 
 export const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="flex min-h-screen flex-col bg-background">
-      <header className="fixed inset-x-0 top-0 z-50 flex items-center justify-between bg-primary p-4 opacity-80 shadow-lg">
-        <h1 className="text-2xl font-extrabold text-primary-foreground">
+    <div className="flex min-h-screen flex-col bg-gradient-to-r from-blue-50 to-blue-100 dark:from-gray-900 dark:to-gray-800">
+      <header className="fixed inset-x-0 top-0 z-50 flex items-center justify-between bg-opacity-80 p-4 shadow-lg backdrop-blur-md">
+        <h1 className="text-2xl font-extrabold text-gray-900 dark:text-white">
           <Link href={"/"}>YKARMR BLOG</Link>
         </h1>
         <ThemeToggle />
       </header>
-      <main className="container mx-auto mt-20 flex-1">{children}</main>
+      <main className="container mx-auto mt-24 flex-1 p-4">{children}</main>
     </div>
   );
 };
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -15,13 +15,13 @@ export function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <SunIcon className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="outline" size="icon" className="relative">
+          <SunIcon className="h-6 w-6 rotate-0 scale-100 transition-transform dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-6 w-6 rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="bg-white dark:bg-gray-800">
         <DropdownMenuItem onClick={() => setTheme("light")}>
           Light
         </DropdownMenuItem>
