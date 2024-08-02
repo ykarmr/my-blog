b9e5924693ed3017bf6c467c69f73077b630c2e1
ykarmr <yuki.arimura0508@gmail.com>
2024-08-02 15:50:21 +0900

    レイアウト修正

--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -6,6 +6,7 @@ import rehypeKatex from "rehype-katex";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import { MdSyntaxHighlighter } from "./MdSyntaxHighlighter";
+import { cn } from "@/lib/utils";
 type Props = {
   markdown: string;
 };
@@ -13,11 +14,34 @@ type Props = {
 function Markdown({ markdown }: Props) {
   return (
     <ReactMarkdown
+      className={cn([
+        "max-w-full",
+        "prose dark:prose-invert",
+        // Table overall styles
+        "prose-table:border-separate prose-table:rounded-lg prose-table:overflow-hidden prose-table:shadow-md",
+        "prose-table:border prose-table:border-gray-200 dark:prose-table:border-gray-700",
+
+        // Table header styles
+        "prose-th:bg-gray-100 dark:prose-th:bg-gray-800 prose-th:text-left prose-th:font-semibold",
+        "prose-th:border prose-th:border-gray-200 dark:prose-th:border-gray-700",
+        "prose-th:px-6 prose-th:py-3",
+
+        // Table data cell styles
+        "prose-td:border prose-td:border-gray-200 dark:prose-td:border-gray-700",
+        "prose-td:px-6 prose-td:py-4 prose-td:bg-white dark:prose-td:bg-gray-900",
+      ])}
       remarkPlugins={[remarkGfm, remarkMath]}
       rehypePlugins={[rehypeKatex, rehypeSlug, rehypeToc]}
       components={{
         code: (props) => {
           return <MdSyntaxHighlighter {...props} />;
+        },
+        table: (props) => {
+          return (
+            <div className="overflow-x-scroll">
+              <table {...props} />
+            </div>
+          );
         },
       }}
     >
--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -5,7 +5,6 @@ import "katex/dist/katex.min.css";
 import { Badge } from "@/components/ui/badge";
 import Meta from "@/components/Meta";
 import Markdown from "@/components/Markdown";
-import { cn } from "@/lib/utils";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const database = await getDatabase();
@@ -43,25 +42,7 @@ const Post = ({
   return (
     <>
       <Meta pageTitle={title} pagePath={`/posts/${pageId}`} />
-      <article
-        className={cn([
-          "mx-auto w-full",
-          "prose dark:prose-invert",
-          // Table overall styles
-          "prose-table:border-separate prose-table:rounded-lg prose-table:overflow-hidden prose-table:shadow-md",
-          "prose-table:border prose-table:border-gray-200 dark:prose-table:border-gray-700",
-          "prose-table:w-full",
-
-          // Table header styles
-          "prose-th:bg-gray-100 dark:prose-th:bg-gray-800 prose-th:text-left prose-th:font-semibold",
-          "prose-th:border prose-th:border-gray-200 dark:prose-th:border-gray-700",
-          "prose-th:px-6 prose-th:py-3",
-
-          // Table data cell styles
-          "prose-td:border prose-td:border-gray-200 dark:prose-td:border-gray-700",
-          "prose-td:px-6 prose-td:py-4 prose-td:bg-white dark:prose-td:bg-gray-900",
-        ])}
-      >
+      <article className="flex flex-col items-center justify-center space-y-4">
         {tags.some((item: any) => item.name === "Notion AI") && (
           <div className="rounded bg-red-100 p-4 text-red-700 dark:bg-red-900 dark:text-red-200">
             Notion AIで作成された記事です。
